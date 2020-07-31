5de7c38fa9030e255d36a0de25808c75c8c9c0ec
rajman01 <58732857+rajman01@users.noreply.github.com>
2020-07-31 03:19:48 +0100

    Update routes.py

--- flack/routes.py
+++ flack/routes.py
@@ -50,10 +50,10 @@ def contact():
 
 
 @app.errorhandler(404)
-def error_404():
+def error_404(error):
     return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
-def error_500():
+def error_500(error):
     return render_template('500.html'), 500
